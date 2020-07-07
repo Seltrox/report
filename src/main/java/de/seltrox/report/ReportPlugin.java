e2790a55cb2637ea3c6d1ef4b7785eb3d9249228
Seltrox <seltroxgaws@gmail.com>
2020-07-07 22:09:36 +0200

    Add bungeecord dependency and bungee.yml

---
+++ src/main/java/de/seltrox/report/ReportPlugin.java
@@ -0,0 +1,25 @@
+/*
+ * (C) Copyright 2020, Jan Benz, http://seltrox.de.
+ *
+ * This software is released under the terms of the Unlicense.
+ * See https://unlicense.org/
+ * for more information.
+ *
+ */
+
+package de.seltrox.report;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ReportPlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
